b278f9a705ebc8f465f3473158f424878ffb270f
SantiMorgan <santiagomorganti01@gmail.com>
2024-07-09 17:47:25 -0300

    Modificación corrección texto

--- src/app/components/pagos-multiples/pagos-selection/pagos-selection.component.ts
+++ src/app/components/pagos-multiples/pagos-selection/pagos-selection.component.ts
@@ -31,9 +31,9 @@ export class PagosSelectionComponent implements OnInit {
 
     this.items = [
       { id: 1, description: 'Pago de Beneficios', buttonText: 'Pago de Beneficios', link: '/pagosImport/1', active: true },
-      { id: 2, description: 'Pago de Haberes', buttonText: 'Pago de Haberes', link: '/pagosImport/1', active: false },
-      { id: 3, description: 'Pago a Proveedores', buttonText: 'Pago a Proveedores', link: '/pagosImport/2', active: false },
-      { id: 4, description: 'Pago de Honorarios', buttonText: 'Pago de Honorarios', link: '/pagosImport/3', active: false },
+      { id: 2, description: 'Pago de Haberes', buttonText: 'Pago de Haberes', link: '/pagosImport/2', active: true },
+      { id: 3, description: 'Pago a Proveedores', buttonText: 'Pago a Proveedores', link: '/pagosImport/3', active: true },
+      { id: 4, description: 'Pago de Honorarios', buttonText: 'Pago de Honorarios', link: '/pagosImport/4', active: true },
       { id: 5, description: 'Embargos (Banco Provincia)', buttonText: 'Embargos (Banco Provincia)', link: '/pagosImport/5', active: false },
       { id: 6, description: 'Embargos (Otros Bancos)', buttonText: 'Embargos (Otros Bancos)', link: '/pagosImport/6', active: false }
     ];
