ce311b994e74a66eed5a52931060b50af826aa41
arenazl <arenazl@gmail.com>
2024-09-05 14:19:51 -0300

    Remove global.ts from tracking

--- src/environments/global.ts
+++
@@ -1,6 +0,0 @@
-
-export const GlobalVariable = Object.freeze({  
-     //BASE_API_URL: 'https://bsi-back-dev-22264eab9225.herokuapp.com/api' 
-     BASE_API_URL: 'https://bsi-back-6f59f9b7097f.herokuapp.com/api'
-    //BASE_API_URL: 'http://localhost:3000/api'
-});
