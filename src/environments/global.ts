bc58f6aece091c17dad536cb995a60f941339f10
arenazl <arenazl@gmail.com>
2024-10-18 05:54:56 -0300

    ok

--- src/environments/global.ts
+++ src/environments/global.ts
@@ -1,9 +1,9 @@
 
 export const GlobalVariable = Object.freeze({  
 
-    //BASE_API_URL: 'https://bsi-back-dev-22264eab9225.herokuapp.com/api'               ////// DEV 
+    BASE_API_URL: 'https://bsi-back-dev-22264eab9225.herokuapp.com/api'               ////// DEV 
     //BASE_API_URL: 'https://bsi-back-6f59f9b7097f.herokuapp.com/api'                   ////// PROD
-    BASE_API_URL: 'http://localhost:3000/api'                                        ////// LOCAL
+    //BASE_API_URL: 'http://localhost:3000/api'                                        ////// LOCAL
  
 });
 
