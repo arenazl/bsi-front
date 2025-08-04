20c571f94f0f527e0e61658285992583c1717682
Lucas Ezequiel Mariano Arenaz <LArenaz@bpba.com.ar>
2025-08-04 08:43:03 -0300

    ok

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000/api',
+  apiUrl: 'https://bsi-back-new-bee31231bd56.herokuapp.com/api',
   appName: 'BSI Sistema de Gestión de Pagos (DEV)',
   appVersion: '2.0.0-dev',
 
